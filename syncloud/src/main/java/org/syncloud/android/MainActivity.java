ddb555efe23ba46db59372bb39d24a9ca64f3505
Boris Rybalkin <ribalkin@gmail.com>
2014-04-09 01:48:23 +0100

    trying jmdns

--- syncloud/src/main/java/org/syncloud/android/MainActivity.java
+++ syncloud/src/main/java/org/syncloud/android/MainActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.net.Uri;
 import android.net.nsd.NsdManager;
 import android.net.nsd.NsdServiceInfo;
+import android.os.AsyncTask;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -12,6 +13,13 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
+
+import java.io.IOException;
+
+import javax.jmdns.JmDNS;
+import javax.jmdns.ServiceEvent;
+import javax.jmdns.ServiceInfo;
+import javax.jmdns.ServiceListener;
 
 public class MainActivity extends ActionBarActivity {
 
@@ -22,10 +30,9 @@ public class MainActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final NsdManager mNsdManager = (NsdManager) getSystemService(Context.NSD_SERVICE);
 
 
-        discoveryListener = new NsdManager.DiscoveryListener() {
+        /*discoveryListener = new NsdManager.DiscoveryListener() {
             @Override
             public void onStartDiscoveryFailed(String s, int i) {
 
@@ -52,12 +59,12 @@ public class MainActivity extends ActionBarActivity {
                 final TextView url = (TextView) findViewById(R.id.url);
 //                url.setText(info.getServiceName() + " on " + info.getHost() + ":" + info.getPort());
 
-                /*runOnUiThread(new Runnable() {
+                *//*runOnUiThread(new Runnable() {
                     @Override
                     public void run() {
 
                     }
-                });*/
+                });*//*
 
 //                Log.d("syncloud", "Service resolved: " + info.getServiceName() + " host:" + info.getHost() + " port:"
 //                        + info.getPort() + " type:" + info.getServiceType());
@@ -69,12 +76,18 @@ public class MainActivity extends ActionBarActivity {
                     }
 
                     @Override
-                    public void onServiceResolved(NsdServiceInfo nsdServiceInfo) {
+                    public void onServiceResolved(final NsdServiceInfo nsdServiceInfo) {
 
-                        url.setText(info.getServiceName() + " on " + info.getHost() + ":" + info.getPort());
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                url.setText(nsdServiceInfo.getServiceName() + " on " + nsdServiceInfo.getHost() + ":" + nsdServiceInfo.getPort());
+                            }
+                        });
 
-                        Log.d("syncloud", "Service resolved: " + info.getServiceName() + " host:" + info.getHost() + " port:"
-                                + info.getPort() + " type:" + info.getServiceType());
+
+//                        Log.d("syncloud", "Service resolved: " + info.getServiceName() + " host:" + info.getHost() + " port:"
+//                                + info.getPort() + " type:" + info.getServiceType());
 
                             mNsdManager.stopServiceDiscovery(discoveryListener);
 
@@ -90,11 +103,96 @@ public class MainActivity extends ActionBarActivity {
             public void onServiceLost(NsdServiceInfo nsdServiceInfo) {
 
             }
+        };*/
+
+
+        AsyncTask<String, Void, Void> asyncTask = new AsyncTask<String, Void, Void>() {
+
+            ServiceListener listener = new ServiceListener() {
+
+                @Override
+                public void serviceAdded(final ServiceEvent event) {
+                    if (event.getName().equals("ownCloud")) {
+                        Log.d("s", "add: " + event.getType());
+                        Log.d("s", "add: " + event.getInfo().getPort());
+                        Log.d("s", "add: " + event.getInfo().getServer());
+
+                        final TextView url = (TextView) findViewById(R.id.url);
+
+                        /*runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                url.setText(event.getType());
+                            }
+                        });*/
+
+                        final ServiceInfo info = event.getDNS().getServiceInfo(event.getType(), event.getName());
+
+                        Log.d("s", "more: " + info.getType());
+//                        Log.d("s", "more: " + info.getInet4Addresses()[0].getHostAddress());
+
+                        String server = info.getServer();
+                        String local = ".local.";
+                        if (server.endsWith(local))
+                            server = server.substring(0, server.length() - local.length());
+
+                        final String text = "URL: http://" + server + ":" + info.getPort() + info.getNiceTextString();
+
+                        Log.d("s", "text: " + text);
+
+
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                url.setText(text);
+                            }
+                        });
+
+                    }
+
+                }
+
+                @Override
+                public void serviceRemoved(ServiceEvent event) {
+                    Log.d("s", "removed: " + event.getType() + "" + event.getName());
+
+                }
+
+                @Override
+                public void serviceResolved(ServiceEvent event) {
+                    if (event.getName().equals("ownCloud")) {
+                        Log.d("s", "resolved: " + event.getType());
+                        Log.d("s", "resolved: " + event.getInfo().getPort());
+                        Log.d("s", "resolved: " + event.getInfo().getServer());
+                    }
+                }
+
+            };
+
+            @Override
+            protected Void doInBackground(String... voids) {
+                JmDNS jmdns = null;
+                try {
+                    jmdns = JmDNS.create();
+                    jmdns.addServiceListener("_http._tcp.local.", listener);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                return null;
+            }
         };
+        asyncTask.execute("");
 
-        mNsdManager.discoverServices("_http._tcp", NsdManager.PROTOCOL_DNS_SD, discoveryListener);
 
+        /*new Thread(new Runnable() {
+            @Override
+            public void run() {
+                final NsdManager mNsdManager = (NsdManager) getSystemService(Context.NSD_SERVICE);
 
+//                mNsdManager.discoverServices("_http._tcp", NsdManager.PROTOCOL_DNS_SD, discoveryListener);
+
+            }
+        }).start();*/
 
     }
 
