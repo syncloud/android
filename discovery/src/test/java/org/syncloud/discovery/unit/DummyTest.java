8dea4e19ad5ab00e6cf2e777fe419c833989ab1c
Boris Rybalkin <ribalkin@gmail.com>
2014-08-10 17:32:42 +0100

    adding dummy test for discovery to exclude integration from running in CI

---
+++ discovery/src/test/java/org/syncloud/discovery/unit/DummyTest.java
@@ -0,0 +1,13 @@
+package org.syncloud.discovery.unit;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+
+public class DummyTest {
+
+    @Test
+    public void test() {
+        Assert.assertTrue(true);
+    }
+}
