f134874314461d7bfb3d478b2a7d3eaa98a9f553
Boris Rybalkin <ribalkin@gmail.com>
2014-08-25 02:12:03 +0100

    fixing integration test

--- ssh/src/test/java/org/syncloud/ssh/integration/EndpointCheckerTest.java
+++ ssh/src/test/java/org/syncloud/ssh/integration/EndpointCheckerTest.java
@@ -4,6 +4,7 @@ import com.google.common.base.Optional;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.syncloud.common.model.Result;
 import org.syncloud.ssh.EndpointChecker;
 import org.syncloud.ssh.model.DirectEndpoint;
 
@@ -11,7 +12,7 @@ public class EndpointCheckerTest {
 
     @Test
     public void testDnsSrv() {
-        Optional<DirectEndpoint> directEndpointOptional = EndpointChecker.dnsService("testdomain1.syncloud.info", "_ssh._tcp", null);
-        Assert.assertTrue(directEndpointOptional.isPresent());
+        Result<DirectEndpoint> directEndpointOptional = EndpointChecker.dnsService("testdomain1.syncloud.info", "_ssh._tcp", null);
+        Assert.assertFalse(directEndpointOptional.hasError());
     }
 }
