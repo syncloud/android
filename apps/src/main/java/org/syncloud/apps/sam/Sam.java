520a385ef377137163fb71b2c61e6143a1f0661c
vsapronov <vladimir.sapronov@gmail.com>
2014-11-22 00:38:18 -0500

    Added few methods to sam

--- apps/src/main/java/org/syncloud/apps/sam/Sam.java
+++ apps/src/main/java/org/syncloud/apps/sam/Sam.java
@@ -1,5 +1,6 @@
 package org.syncloud.apps.sam;
 
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import org.syncloud.common.progress.Progress;
@@ -9,7 +10,8 @@ import org.syncloud.ssh.model.Device;
 
 import java.util.List;
 
-import static org.syncloud.apps.sam.Command.List;
+import static java.util.Arrays.asList;
+import static org.apache.commons.lang3.StringUtils.join;
 import static org.syncloud.apps.sam.Command.Update;
 import static org.syncloud.common.model.Result.value;
 
@@ -24,8 +26,29 @@ public class Sam {
         this.progress = progress;
     }
 
-    public Result<String> run(Device device, Command command, String... arguments) {
+    private String cmd(String... arguments) {
+        List<String> cmd = asList(arguments);
+        cmd.add(0, "sam");
 
+        return join(cmd, " ");
+    }
+
+    public <TContent> Result<TContent> runTyped(Device device, String... arguments) {
+        return run(device, arguments).flatMap(new Result.Function<String, Result<TContent>>() {
+            @Override
+            public Result<TContent> apply(String v) throws Exception {
+                TContent content = JSON.readValue(v, new TypeReference<TContent>() {});
+                return value(content);
+            }
+        });
+    }
+
+    public Result<String> run(Device device, String... arguments) {
+        String command = cmd(arguments);
+        return ssh.execute(device, command);
+    }
+
+    public Result<String> run(Device device, Command command, String... arguments) {
         return ssh.execute(device, command.cmd(arguments));
     }
 
@@ -36,7 +59,7 @@ public class Sam {
 
     public Result<List<AppVersions>> list(Device device) {
         progress.title("Refreshing app list");
-        return appList(device, List);
+        return runTyped(device, Commands.list);
     }
 
     private Result<List<AppVersions>> appList(Device device, Command command, String... arguments) {
