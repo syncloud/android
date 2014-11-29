d63c18253417544398e8a69b67110a7838ebba90
vsapronov <vladimir.sapronov@gmail.com>
2014-11-29 15:35:29 -0500

    Error reporting dialog in Holo theme, no input asked

--- syncloud/src/main/java/org/syncloud/android/SyncloudApplication.java
+++ syncloud/src/main/java/org/syncloud/android/SyncloudApplication.java
@@ -33,9 +33,8 @@ import static org.acra.ReportField.*;
         customReportContent = {USER_COMMENT, APP_VERSION_CODE, ANDROID_VERSION, PHONE_MODEL, CUSTOM_DATA, STACK_TRACE, LOGCAT },
         resToastText = R.string.crash_toast_text, // optional, displayed as soon as the crash occurs, before collecting data which can take a few seconds
         resDialogText = R.string.crash_dialog_text,
-        resDialogIcon = android.R.drawable.ic_dialog_info, //optional. default is a warning sign
+        resDialogIcon = R.drawable.ic_launcher, //optional. default is a warning sign
         resDialogTitle = R.string.crash_dialog_title, // optional. default is your application name
-        resDialogCommentPrompt = R.string.crash_dialog_comment_prompt, // optional. when defined, adds a user text field input with this text resource as a label
         resDialogOkToast = R.string.crash_dialog_ok_toast, // optional. displays a Toast message when the user accepts to send a report.
         logcatArguments = { "-t", "200", "-v", "long", "*:D"},
         logcatFilterByPid = false
