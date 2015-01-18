a36eaf0f669d6c4171c28ef762ccb7b3a935c0c1
vsapronov <vladimir.sapronov@gmail.com>
2015-01-17 23:38:42 -0500

    Import of ImageButton

--- syncloud/src/main/java/org/syncloud/android/ui/adapters/DevicesSavedAdapter.java
+++ syncloud/src/main/java/org/syncloud/android/ui/adapters/DevicesSavedAdapter.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
