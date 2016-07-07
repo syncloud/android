0931db322d44c3df0cd96399c5ff48d172632353
Vladimir Sapronov <vladimir.sapronov@gmail.com>
2016-07-06 21:49:43 -0400

    Using standard trust manager when possible

--- syncloud/src/main/java/org/syncloud/android/network/SyncloudSSLSocketFactory.java
+++ syncloud/src/main/java/org/syncloud/android/network/SyncloudSSLSocketFactory.java
@@ -15,25 +15,53 @@ import java.security.cert.X509Certificate;
 
 import javax.net.ssl.SSLContext;
 import javax.net.ssl.TrustManager;
+import javax.net.ssl.TrustManagerFactory;
 import javax.net.ssl.X509TrustManager;
 
 public class SyncloudSSLSocketFactory extends SSLSocketFactory {
+
+    public class EasyX509TrustManager implements X509TrustManager {
+
+        private X509TrustManager standardTrustManager = null;
+
+        public EasyX509TrustManager(KeyStore keystore) throws NoSuchAlgorithmException, KeyStoreException {
+            super();
+            TrustManagerFactory factory = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
+            factory.init(keystore);
+            TrustManager[] trustmanagers = factory.getTrustManagers();
+            if (trustmanagers.length == 0) {
+                throw new NoSuchAlgorithmException("no trust manager found");
+            }
+            this.standardTrustManager = (X509TrustManager) trustmanagers[0];
+        }
+
+        public void checkClientTrusted(X509Certificate[] certificates, String authType) throws CertificateException {
+            standardTrustManager.checkClientTrusted(certificates, authType);
+        }
+
+        public void checkServerTrusted(X509Certificate[] certificates, String authType) throws CertificateException {
+            if ((certificates != null) && (certificates.length == 1 || certificates.length == 2)) {
+                certificates[0].checkValidity();
+                if (certificates.length == 2)
+                    certificates[1].checkValidity();
+            } else {
+                standardTrustManager.checkServerTrusted(certificates, authType);
+            }
+        }
+
+        public X509Certificate[] getAcceptedIssuers() {
+            return this.standardTrustManager.getAcceptedIssuers();
+        }
+
+    }
+
+
     SSLContext sslContext = SSLContext.getInstance("TLS");
 
     public SyncloudSSLSocketFactory(KeyStore truststore) throws NoSuchAlgorithmException, KeyManagementException, KeyStoreException, UnrecoverableKeyException {
         super(truststore);
 
-        TrustManager tm = new X509TrustManager() {
-            public void checkClientTrusted(X509Certificate[] chain, String authType) throws CertificateException {
-            }
-
-            public void checkServerTrusted(X509Certificate[] chain, String authType) throws CertificateException {
-            }
-
-            public X509Certificate[] getAcceptedIssuers() {
-                return null;
-            }
-        };
+        TrustManager tm = new EasyX509TrustManager(truststore);
 
         sslContext.init(null, new TrustManager[] { tm }, null);
     }
