52d999f9a2344cd3f879c1cd468ccde2cdcfb56e
Billy Leung <82518709+bleunguts@users.noreply.github.com>
2021-08-25 18:19:03 +0100

    Added Kotlin Plugin support to syncloud

---
+++ syncloud/src/test/java/org/syncloud/android/FooTest.kt
@@ -0,0 +1,12 @@
+package org.syncloud.android.ui
+
+import junit.framework.Assert
+import org.junit.Test
+
+class FooTest {
+    @Test
+    fun itAllWorks() {
+        Assert.assertTrue("It builds and runs in kotlin.", true)
+    }
+}
+
