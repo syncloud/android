dd13decf54d36048281a2e52e11ea4790c77c58c
Boris Rybalkin <ribalkin@gmail.com>
2021-10-23 21:20:49 +0100

    migrate progress async task to kotlin coroutine

--- syncloud/src/main/java/org/syncloud/android/ui/DevicesSavedActivity.kt
+++ syncloud/src/main/java/org/syncloud/android/ui/DevicesSavedActivity.kt
@@ -3,24 +3,25 @@ package org.syncloud.android.ui
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
-import android.preference.PreferenceManager
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
 import android.widget.AdapterView
 import android.widget.ListView
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import com.google.android.material.floatingactionbutton.FloatingActionButton
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import org.syncloud.android.Preferences
-import org.syncloud.android.Progress
 import org.syncloud.android.R
 import org.syncloud.android.SyncloudApplication
 import org.syncloud.android.core.platform.model.DomainModel
-import org.syncloud.android.core.redirect.model.User
 import org.syncloud.android.core.redirect.model.toModels
-import org.syncloud.android.tasks.AsyncResult
-import org.syncloud.android.tasks.ProgressAsyncTask
 import org.syncloud.android.ui.adapters.DevicesSavedAdapter
 
 class DevicesSavedActivity : AppCompatActivity() {
@@ -31,22 +32,22 @@ class DevicesSavedActivity : AppCompatActivity() {
     private lateinit var swipeRefreshLayout: SwipeRefreshLayout
     private lateinit var btnDiscovery: FloatingActionButton
     private lateinit var emptyView: View
-    private val progress: Progress = ProgressImpl()
+    private lateinit var activityLauncher: ActivityResultLauncher<Intent>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val supportActionBar = supportActionBar
-        supportActionBar?.setDisplayShowHomeEnabled(true)
         setContentView(R.layout.activity_devices_saved)
-        PreferenceManager.setDefaultValues(this, R.xml.preferences, false)
         emptyView = findViewById(android.R.id.empty)
         listview = findViewById(R.id.devices_saved)
-        listview.setOnItemClickListener { _: AdapterView<*>?, _: View?, position: Int, l: Long ->
+        listview.setOnItemClickListener { _: AdapterView<*>?, _: View?, position: Int, _: Long ->
             val obj = listview.getItemAtPosition(position)
             val domain = obj as DomainModel
             open(domain)
         }
         btnDiscovery = findViewById(R.id.discovery_btn)
+        btnDiscovery.setOnClickListener {
+            activityLauncher.launch(Intent(this, DevicesDiscoveryActivity::class.java))
+        }
         adapter = DevicesSavedAdapter(this)
         listview.adapter = adapter
         application = getApplication() as SyncloudApplication
@@ -55,6 +56,10 @@ class DevicesSavedActivity : AppCompatActivity() {
         swipeRefreshLayout.setColorSchemeResources(R.color.logo_blue, R.color.logo_green)
         swipeRefreshLayout.setOnRefreshListener { refreshDevices() }
         swipeRefreshLayout.post { refreshDevices() }
+
+        activityLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+            refreshDevices()
+        }
     }
 
     private fun refreshDevices() {
@@ -65,45 +70,32 @@ class DevicesSavedActivity : AppCompatActivity() {
             emptyView.visibility = View.GONE
             listview.emptyView = null
             adapter.clear()
-            ProgressAsyncTask<Void, User>()
-                    .setProgress(progress)
-                    .doWork(object : ProgressAsyncTask.Work<Void, User> {
-                        override fun run(vararg args: Void): User {
-                            return userService.getUser(redirectEmail, redirectPassword)!!
-                        }
-                    })
-                    .onCompleted(object : ProgressAsyncTask.Completed<User> {
-                        override fun run(result: AsyncResult<User>?) {
-                            emptyView.visibility = View.VISIBLE
-                            listview.emptyView = emptyView
-                        }
-                    })
-                    .onSuccess(object : ProgressAsyncTask.Success<User> {
-                        override fun run(result: User?) {
-                            updateUser(result)
-                        }
-                    })
-                    .execute()
 
-        }
-    }
+            swipeRefreshLayout.isRefreshing = true
+            listview.isEnabled = false
+            btnDiscovery.visibility = View.GONE
 
-    private fun updateUser(user: User?) {
-        val domains = user?.domains?.toModels() ?: listOf()
-        val sortedDomains = domains.sortedWith{ first: DomainModel, second: DomainModel ->
-            first.name.compareTo(second.name)
+            CoroutineScope(Dispatchers.IO).launch {
+                val user = userService.getUser(redirectEmail, redirectPassword)
+                val domains = user?.domains?.toModels() ?: listOf()
+                val sortedDomains = domains.sortedWith{ first, second ->
+                    first.name.compareTo(second.name)
+                }
+                withContext(Dispatchers.Main) {
+                    adapter.clear()
+                    adapter.addAll(sortedDomains)
+                    swipeRefreshLayout.isRefreshing = false
+                    listview.isEnabled = true
+                    btnDiscovery.visibility = View.VISIBLE
+                    emptyView.visibility = View.VISIBLE
+                    listview.emptyView = emptyView
+                }
+            }
         }
-        adapter.clear()
-        adapter.addAll(sortedDomains)
     }
 
     private fun open(device: DomainModel) {
         startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(device.dnsUrl())))
-    }
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        refreshDevices()
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -114,28 +106,8 @@ class DevicesSavedActivity : AppCompatActivity() {
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         val id = item.itemId
         if (id == R.id.action_settings) {
-            startActivityForResult(Intent(this, SettingsActivity::class.java), 2)
+            activityLauncher.launch(Intent(this, SettingsActivity::class.java))
         }
         return super.onOptionsItemSelected(item)
-    }
-
-    fun discover(view: View?) {
-        startActivityForResult(Intent(this, DevicesDiscoveryActivity::class.java), 1)
-    }
-
-    inner class ProgressImpl : Progress.Empty() {
-        override fun start() {
-            swipeRefreshLayout.isRefreshing = true
-            listview.isEnabled = false
-            btnDiscovery.visibility = View.GONE
-        }
-
-        override fun stop() {
-            swipeRefreshLayout.isRefreshing = false
-            listview.isEnabled = true
-            btnDiscovery.visibility = View.VISIBLE
-        }
-
-        override fun error(message: String?) {}
     }
 }
